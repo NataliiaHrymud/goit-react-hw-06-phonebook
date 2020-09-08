ed1359f0051d8687859f5c9852dfdcb5ba56fbca
Nataliia Hrymud <ngrimud@gmail.com>
2020-09-08 23:05:54 +0300

    Updates

---
+++ precache-manifest.4e9a10f40a1455cde39ca1d2fc0a14c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8174b144d9524c01de217bcb56c7417",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "1dcbec0cab004b2834eb",
+    "url": "/goit-react-hw-06-phonebook/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "01d418f5c2f88e4998e6",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.ffa08808.chunk.css"
+  },
+  {
+    "revision": "1dcbec0cab004b2834eb",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.81931ec9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.81931ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01d418f5c2f88e4998e6",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.4c7b5678.chunk.js"
+  },
+  {
+    "revision": "9d1a26fb56dbe9d98a08",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b0a8c527.js"
+  }
+]);
